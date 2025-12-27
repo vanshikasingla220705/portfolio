9c5d2e0b607e671f80bc1c8a59f9ddaca1fcac59
vanshikasingla220705 <vanshikasinglakkr@gmail.com>
2025-12-27 23:21:49 +0530

    ab

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { motion } from "framer-motion";
+
+export default function Header() {
+  return (
+    <header>
+      <motion.div className=' top-0 left-0 h-800 w-full bg-red-500'>
+        header working
+      </motion.div>
+    </header>
+  );
+}
